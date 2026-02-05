8fbe73339cad0badd721a56c75aaee2e15bd0f88
lureilly1 <lureilly1@gmail.com>
2026-02-05 20:07:18 +0100

    remove git ignore and vite changes

--- examples/vite-starter/vite.config.ts
+++ examples/vite-starter/vite.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@hypequery/react': path.resolve(__dirname, '../../packages/react/dist'),
-      '@hypequery/serve': path.resolve(__dirname, '../../packages/serve/dist'),
-    },
-  },
   server: {
     proxy: {
       '/api': {
